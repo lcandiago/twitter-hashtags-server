abdd7774655c9eab9e3a1b5f1a507c6d05d60b03
Lorenzo Candiago <lorenzo@goodsbr.com.br>
2020-02-20 17:32:01 -0300

    improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,9 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
